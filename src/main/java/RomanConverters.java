2d28f796eb8cfb114450d91f2cc8f8686e81e697
bg64 <Simon.Read@uk.tesco.com>
2017-04-27 15:48:09 +0100

    refactored to include enum

--- src/main/java/RomanConverters.java
+++ src/main/java/RomanConverters.java
@@ -2,15 +2,22 @@
 public class RomanConverters {
     public static String convert(int number) {
         String roman = "";
-        int[] arabic_numerals = {10, 9, 5, 4, 1};
-        String[] roman_numerals = {"X", "IX", "V", "IV", "I"};
+        //int[] arabic_numerals = {10, 9, 5, 4, 1};
+        //String[] roman_numerals = {"X", "IX", "V", "IV", "I"};
 
-        for (int i = 0; i < arabic_numerals.length; i++) {
-            while (number >= arabic_numerals[i]) {
-                roman += roman_numerals[i];
-                number -= arabic_numerals[i];
+        for(RomanNumerals romanNumeral : RomanNumerals.values()){
+            while ( number >= romanNumeral.arab) {
+                roman += romanNumeral.name();
+                number -= romanNumeral.arab;
             }
         }
+
+        /*for (int i = 0; i < RomanNumerals. arabic_numerals.length; i++) {
+            while (number >= arabic_numerals[i]) {
+                roman += RomanNumerals.;
+                number -= arabic_numerals[i];
+            }
+        }*/
         return roman;
     }
 }
--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -1,4 +1,10 @@
 
 public enum RomanNumerals {
-    V, X
+    X(10), IX(9), V(5), IV(4), I(1);
+
+    int arab;
+
+    RomanNumerals(int arab) {
+        this.arab = arab;
+    }
 }
