205ee4ca46b2eb55d488914b01a49329e0d0f178
bg64 <Simon.Read@uk.tesco.com>
2017-06-19 08:59:31 +0100

    *correct code and add comments to work with or without enum

--- src/main/java/RomanConverters.java
+++ src/main/java/RomanConverters.java
@@ -2,22 +2,25 @@
 public class RomanConverters {
     public static String convert(int number) {
         String roman = "";
-        //int[] arabic_numerals = {10, 9, 5, 4, 1};
-        //String[] roman_numerals = {"X", "IX", "V", "IV", "I"};
+        int[] arabic_numerals = {10, 9, 5, 4, 1};
+        String[] roman_numerals = {"X", "IX", "V", "IV", "I"};
 
-        for(RomanNumerals romanNumeral : RomanNumerals.values()){
-            while ( number >= romanNumeral.arab) {
+        /*for(RomanNumerals romanNumeral : RomanNumerals.values()){
+            while (number >= romanNumeral.arab) {
                 roman += romanNumeral.name();
                 number -= romanNumeral.arab;
             }
-        }
+        }*/
 
-        /*for (int i = 0; i < RomanNumerals. arabic_numerals.length; i++) {
+        //comment out this method (and arabic/roman_numerals variables above) to use enum
+        for (int i = 0; i < arabic_numerals.length; i++) {
             while (number >= arabic_numerals[i]) {
-                roman += RomanNumerals.;
+                //you can only increment (+=) a number, therefore, had to concat the string below
+                roman = roman.concat(roman_numerals[i]);
                 number -= arabic_numerals[i];
             }
-        }*/
+        }
+
         return roman;
     }
 }
